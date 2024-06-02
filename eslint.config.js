9e63d5d331439f6d0d54862ffb67006ee2fcce73
Evgeniya Smirnova <evgeniyasmirnova89@gmail.com>
2024-06-02 23:54:37 +0300

    Testing

---
+++ eslint.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    languageOptions: {
+        globals: {
+            browser: true,
+            es2021: true
+        }
+    },
+    rules: {
+        semi: ["warn", "always"],
+        quotes: ["warn", "double"]
+    }
+};
--- src/index.js
+++ src/index.js
@@ -2,8 +2,5 @@ import Fish from './Fish.js'
 
 const fish = new Fish('Webpack Fish Title');
 
-const test = "FAIL";
-const test2 = "FAIL2";
-
 console.log('Fish to String:', fish.toString())
 
